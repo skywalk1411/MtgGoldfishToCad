297ae703bedf91af888a94f03436e0a1b1c5e9c8
skywalk <61213518+skywalk1411@users.noreply.github.com>
2022-05-25 12:12:51 -0400

    Create usdToCadPrices.js

---
+++ usdToCadPrices.js
@@ -0,0 +1,14 @@
+fetch('https://stonkch.art/api2api/https%3A%2F%2Fquery1.finance.yahoo.com%2Fv10%2Ffinance%2FquoteSummary%2FUSDCAD%3DX%3Fmodules%3DsummaryProfile%2Cprice%2CdefaultKeyStatistics%2CfinancialData')
+    .then(res => res.json())
+    .then(data => {
+        for (let element of document.querySelectorAll(".priceList > * > * > * > :nth-child(2)")) {
+            if (element.textContent !== "\nRetail\n\n\n\n" || element.textContent !== "EV") {
+                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            }
+        }
+        for (let element of document.querySelectorAll(".priceList > * > * > * > :nth-child(3)")) {
+            if (element.textContent !== "\nRetail\n\n\n\n" || element.textContent !== "EV") {
+                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            }
+        }
+    });
