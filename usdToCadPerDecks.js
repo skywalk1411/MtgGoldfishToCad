7882d07d78f256385c7f7dffb811b46f71849a7b
skywalk <61213518+skywalk1411@users.noreply.github.com>
2022-05-25 18:25:40 -0400

    Update usdToCadPerDecks.js

--- usdToCadPerDecks.js
+++ usdToCadPerDecks.js
@@ -6,19 +6,21 @@ fetch('https://stonkch.art/api2api/https%3A%2F%2Fquery1.finance.yahoo.com%2Fv10%
         laPrice.textContent = laPrice.textContent + ` (${(Number(laPrice.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
     }
     let laTable = document.querySelector("#tab-paper > div:nth-child(1) > table:nth-child(1)");
-    let laTableLength = laTable.rows.length;
-    for (let i=2;i<=laTableLength;i++) {
-        let element = document.querySelector(`#tab-paper > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(${i}) > td:nth-child(4)`);
-        if (element) {
-            element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+    if (laTable) {
+        for (let i=2;i<=laTable.rows.length;i++) {
+            let element = document.querySelector(`#tab-paper > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(${i}) > td:nth-child(4)`);
+            if (element) {
+                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            }
         }
     }
     laTable = document.querySelector("#tab-paper > div:nth-child(1) > table:nth-child(2)");
-    laTableLength = laTable.rows.length;
-    for (let i=1;i<=laTableLength;i++) {
-        let element = document.querySelector(`#tab-paper > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(${i}) > td:nth-child(4)`);
-        if (element) {
-            element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+    if (laTable) {
+        for (let i=1;i<=laTable.rows.length;i++) {
+            let element = document.querySelector(`#tab-paper > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(${i}) > td:nth-child(4)`);
+            if (element) {
+                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            }
         }
     }
 });
