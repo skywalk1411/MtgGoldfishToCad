dea62a0c178cba08adbf83a3d75327511ab6dbff
skywalk <61213518+skywalk1411@users.noreply.github.com>
2022-05-25 18:26:16 -0400

    Update usdToCadPrices.js

--- usdToCadPrices.js
+++ usdToCadPrices.js
@@ -2,13 +2,17 @@ fetch('https://stonkch.art/api2api/https%3A%2F%2Fquery1.finance.yahoo.com%2Fv10%
     .then(res => res.json())
     .then(data => {
         for (let element of document.querySelectorAll(".priceList > * > * > * > :nth-child(2)")) {
-            if (element.textContent !== "\nRetail\n\n\n\n" || element.textContent !== "EV") {
-                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            if (element.textContent !== "\nRetail\n\n\n\n" && element.textContent !== "EV") {
+                element.textContent = `\n${element.textContent.slice(2,element.textContent.indexOf('\n\n\n\n'))} (${(Number(element.textContent.slice(3,element.textContent.indexOf('\n\n\n\n')).replace(/[^0-9.-]+/g, ""))* Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)\n\n\n\n`;
+                //element.style.whiteSpace = 'nowrap';
+                //element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
             }
         }
         for (let element of document.querySelectorAll(".priceList > * > * > * > :nth-child(3)")) {
-            if (element.textContent !== "\nRetail\n\n\n\n" || element.textContent !== "EV") {
-                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            if (element.textContent !== `\nRetail\n\n\n\n` && element.textContent !== "EV") {
+                element.textContent = `\n${element.textContent.slice(2,element.textContent.indexOf('\n\n\n\n'))} (${(Number(element.textContent.slice(3,element.textContent.indexOf('\n\n\n\n')).replace(/[^0-9.-]+/g, ""))* Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)\n\n\n\n`;
+                //element.style.whiteSpace = 'nowrap';
+                //element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g, "")) * Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
             }
         }
     });
