a6e0bfa991fd25c8483fc3508c15c0fae9cca62a
skywalk <61213518+skywalk1411@users.noreply.github.com>
2022-05-25 18:25:08 -0400

    Update usdToCadMoversDetails.js

--- usdToCadMoversDetails.js
+++ usdToCadMoversDetails.js
@@ -2,11 +2,12 @@ fetch('https://stonkch.art/api2api/https%3A%2F%2Fquery1.finance.yahoo.com%2Fv10%
 .then(res => res.json())
 .then(data=> {
     let laTable = document.querySelector(".table");
-    let laTableLength = laTable.rows.length;
-    for (let i=1;i<=laTableLength;i++) {
-        let element = document.querySelector(`.table > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(4)`);
-        if (element) {
-            element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+    if (laTable) {
+        for (let i=1;i<=laTable.rows.length;i++) {
+            let element = document.querySelector(`.table > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(4)`);
+            if (element) {
+                element.textContent = element.textContent + ` (${(Number(element.textContent.slice(2).replace(/[^0-9.-]+/g,""))*Number(data.quoteSummary.result[0].price.regularMarketPrice.raw)).toFixed(2)}$)`;
+            }
         }
     }
 });
